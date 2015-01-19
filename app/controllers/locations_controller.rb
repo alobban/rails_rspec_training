4c28bc7b09c15ea407fc01e351b41e4285f949da
Andrew Lobban <andrewglobban@gmail.com>
2015-01-19 13:17:42 -0600

    Adds LocationsController tests

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,36 @@
 class LocationsController < ApplicationController
+  def create
+  	@location = Location.new(location_params)
+  	if @location.save
+  	  redirect_to location_path(@location.id)
+  	else
+  	  render action: "new"
+  	end
+  end
+
+  def show
+  	begin
+  	  @location = Location.find(params[:id])
+  	rescue ActiveRecord::RecordNotFound
+  	  render status: 404
+  	end
+  end
+
+  def index
+  	@locations = Location.all
+  end
+
+  def destroy
+  	begin
+  	  Location.destroy(params[:id])
+  	rescue
+  	  render status: 404
+  	end
+  end
+
+  private
+
+  def location_params
+  	params.require(:location).permit(:latitude, :longitude)
+  end
 end
--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe LocationsController, :type => :controller do
+  describe "#create" do
+  	subject { post :create, { location: 
+  	  { latitude: 25.0, 
+  	  	longitude: -40.0 }
+  	  }
+  	}
+  	it "saves the location" do
+  	  subject
+  	  Location.all.count.should == 1
+  	end
+  	it "should redirect to show the created location" do
+  	  subject.should redirect_to(location_path(Location.first.id))
+  	end
+  end
 
+  describe "#new" do
+  	context "when invalid longitude" do
+  	  subject { post :create, { location: 
+  	  	{ latitude: 25.0 } } }
+  	  its(:status) { should == 200 } # OK
+  	  it "should render the new view" do
+  	  	subject
+  	  	response.should render_template("new")
+  	  end
+  	end
+  end
+
+  describe "#show" do
+  	context "when the location exists" do
+  	  let(:location) { Location.create(latitude: 25.0, longitude: -40.0) }
+  	  subject { get :show, id: location.id }
+  	  it "assigns @location" do
+  	  	subject
+  	  	assigns(:location).should eq(location)
+  	  end
+  	  it "renders the show template" do
+  	  	subject
+  	  	response.should render_template("show")
+  	  end
+  	end
+
+  	context "when thel ocation does not exist" do
+  	  subject { get :show, id: 404 }
+  	  its(:status) { should == 404 }
+  	end
+  end
+
+  describe "#index" do
+  	context "when there are some locations" do
+  	  let(:locations) do
+  	  	[
+  	  	  Location.create(latitude: 25.0,
+  	  	  	longitude: -40.0),
+  	  	  Location.create(latitude: -10.0,
+  	  	  	longitude: 42.0)
+  	  	]
+  	  end
+  	  #TODO check with let!
+  	  before { locations }
+  	  subject { get :index }
+  	  it "assigns @locations" do
+  	  	subject # let!
+  	  	assigns(:locations).should eq(locations)
+  	  end
+  	end
+
+  	context "when there are no locations" do
+  	  subject { get :index }
+  	  it "assigns @locations" do
+  	  	subject
+  	  	assigns(:locations).should eq([])
+  	  end
+  	end
+  end
+
+  describe "#destroy" do
+  	context "when the location exists" do
+  	  let (:location) { Location.create(
+  	  	latitude: 25.0, longitude: -40)
+  	  }
+  	  subject { post :destroy, id: location.id }
+  	  it "deletes the location" do
+  	  	subject
+  	  	Location.all.count.should == 0
+  	  end
+  	end
+  end
 end
